e241cb5f6c477887171854e2ad6fdb133a28cbb5
Paul Przybyszewski <pa.przybyszewski@gmail.com>
2022-01-06 23:50:36 +0100

    version number

--- apico/__init__.py
+++ apico/__init__.py
@@ -6,7 +6,7 @@ RESTful API Monitor
 
 A tool for monitoring RESTful APIs
 
-:copyright: (c) 2021 wulf
+:copyright: (c) 2021-present wulf
 :license: MIT, see LICENSE for more details.
 """
 
@@ -15,16 +15,16 @@ from .request import Request
 from requests import Response
 from collections import namedtuple
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __title__ = 'apico'
 __license__ = 'MIT'
 __author__ = 'wulf'
 __email__ = 'wulf.developer@gmail.com'
 __uri__ = "https://github.com/itsmewulf/apico"
-__copyright__ = '(c) 2021 %s' % __author__
+__copyright__ = '(c) 2021-present %s' % __author__
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
 VersionInfo = namedtuple('VersionInfo', 'major minor micro releaselevel serial')
 
-version_info = VersionInfo(major=1, minor=0, micro=0, releaselevel='final', serial=0)
+version_info = VersionInfo(major=1, minor=0, micro=1, releaselevel='final', serial=0)
